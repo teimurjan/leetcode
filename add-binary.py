86e01a6ea95b8b86396b49ea64b9325542fe937a
Teimur Gasanov <teymurgg321@gmail.com>
2021-04-30 13:17:26 +0600

    Add group anagrams + add binary

---
+++ add-binary.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = ''
+
+        i, j = len(a) - 1, len(b) - 1
+
+        carry = 0
+        while i >= 0 or j >= 0:
+            a_i = int(a[i]) if i >= 0 else 0
+            b_j = int(b[j]) if j >= 0 else 0
+
+            sum_ = a_i + b_j + carry
+            carry = 0 if sum_ < 2 else 1
+            result = str(sum_ % 2) + result
+
+            i -= 1
+            j -= 1
+
+        if carry:
+            result = '1' + result
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+
+        self.assertEqual(s.addBinary('1111', '1111'), '11110')
+        self.assertEqual(s.addBinary('1010', '1011'), '10101')
+        self.assertEqual(s.addBinary('11', '1'), '100')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ group-anagrams.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+import unittest
+
+
+class Solution:
+    def serializeStr(self, str: str) -> Tuple[str]:
+        return tuple(sorted(str))
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        for str_ in strs:
+            serialized = self.serializeStr(str_)
+            if seen.get(serialized) is None:
+                seen[serialized] = 1
+            else:
+                seen[serialized] += 1
+
+        groups = {}
+        for str_ in strs:
+            serialized = self.serializeStr(str_)
+            if seen[serialized]:
+                seen[serialized] -= 1
+                if groups.get(serialized):
+                    groups[serialized].append(str_)
+                else:
+                    groups[serialized] = [str_]
+
+        return list(groups.values())
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+
+        self.assertEqual(
+            s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]),
+            [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+        )
+        self.assertEqual(s.groupAnagrams([""]), [[""]])
+        self.assertEqual(s.groupAnagrams(["a"]), [["a"]])
+
+
+if __name__ == '__main__':
+    unittest.main()
